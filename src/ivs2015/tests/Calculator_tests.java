6f89c28994bfae0122e5d3912a48504c156a894b
xcocul00 <marian.cocula@gmail.com>
2016-04-16 22:08:57 +0200

    JUnit

--- src/ivs2015/tests/Calculator_tests.java
+++ src/ivs2015/tests/Calculator_tests.java
@@ -1,14 +1,20 @@
 package ivs2015.tests;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import ivs2015.coculator.*; 
 
 public class Calculator_tests {
 	
 	public static void main (String[] args) {
 		test1();
+		new Coculator();
 	}
 	
 	public static void test1() {
-		System.out.println("Test");
+		System.out.println("Test1");
+		
 	}
 
 }
