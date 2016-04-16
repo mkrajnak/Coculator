40e16b727d7f0d4833e502de9546a3bbef1bc95b
xcocul00 <marian.cocula@gmail.com>
2016-04-16 21:44:26 +0200

    Vytvorenie triedy pre testy

---
+++ src/ivs2015/tests/Calculator_tests.java
@@ -0,0 +1,14 @@
+package ivs2015.tests;
+
+
+public class Calculator_tests {
+	
+	public static void main (String[] args) {
+		test1();
+	}
+	
+	public static void test1() {
+		System.out.println("Test");
+	}
+
+}
